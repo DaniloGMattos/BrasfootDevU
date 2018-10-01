14091072983026ac885d7c3a5ab429d24e7213fe
DaniloGMattos <mattosgdanilo@gmail.com>
2018-10-01 10:59:47 -0300

    [FIX] Pequenas correções no acesso dos scripts

--- BrasfootDev/Assets/Scripts/Team.cs
+++ BrasfootDev/Assets/Scripts/Team.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class Team : MonoBehaviour {
 	public Player player;
-	private enum League{A,B,C,D};
-	private League league;
-	private string teamName;
-	private float power;
-	private float defence;
-	private float crowd;
+	public enum League{A,B,C,D};
+	public League league;
+	public string teamName;
+	public float power;
+	public float defence;
+	public float crowd;
 	// Use this for initialization
 	void Start () {
 		power = Random.Range(0f,100f);
