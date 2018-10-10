15d07e15da85a55c9608b2f74739091ff124f2b2
DaniloGMattos <mattosgdanilo@gmail.com>
2018-10-10 17:34:26 -0300

    [ADD] O nome do tecnico pode ser salvo e aparece no hub

--- BrasfootDev/Assets/Scripts/CoachController.cs
+++ BrasfootDev/Assets/Scripts/CoachController.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoachController : MonoBehaviour {
-	public Coach coach;
+	public static CoachController ThisIsTheOneAndOnlyCoachController;
+	public Coach coach= new Coach();
+	
 
 	// Use this for initialization
 	void Start () {
-		
+		ThisIsTheOneAndOnlyCoachController = this;
+		GameObject.DontDestroyOnLoad(this.gameObject);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+	public static CoachController GetInstance(){
+		return ThisIsTheOneAndOnlyCoachController;
+	}
+	// public void GetCoachName(string newText){
+	// 	coach.coachName = newText;
+	// 	print(coach.coachName);
+	// }
 }
